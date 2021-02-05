9dedc013b9b7e5d47262d991e8da7d347b5e3c34
Angkan <angkan1989@icloud.com>
2021-02-05 10:08:53 +0800

    Initial Commit

---
+++ Whiplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whiplash
+//
+//  Created by Angkan Chanda on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
