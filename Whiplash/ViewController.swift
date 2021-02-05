17a2350311c5fe3d7542e748808163989d23df1a
Angkan <angkan1989@icloud.com>
2021-02-05 11:46:44 +0800

    iOS Whiplash App Initial Commit

--- Whiplash/ViewController.swift
+++ Whiplash/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var ap = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var WhipLabel: UILabel!
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            do {
+                ap = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "crack_the_whip", ofType: "mp3")!))
+                //ap.prepareToPlay()
+                ap.play()
+                print("audio")
+            } catch {
+                print(error)
+            }
+            WhipLabel.text = "Whipped your arse!"
+        }
+    }
+    
 
 }
 
